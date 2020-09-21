137b2afb6a4b153f1c5c55c52242851f3bb9a005
Gabriele <gabrielepese@gmail.com>
2020-09-21 15:24:26 +0200

    ok

---
+++ dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".accordion").click(function () {
+    $(this).toggleClass("active");
+  });
+}); // });
+// $(".bottone-menu").click(function(){
+//     $(".menu2").toggleClass ("active")
+//     $("footer").toggleClass ("hidden")
+//   });
---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".accordion").click(function () {
+      $(this).toggleClass("active")
+  });
+
+});
+
+// });
+
+
+// $(".bottone-menu").click(function(){
+//     $(".menu2").toggleClass ("active")
+//     $("footer").toggleClass ("hidden")
+//   });
