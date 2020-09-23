3ef008129ba35b598d308bbd9312e0a59e9ce749
Gabriele <gabrielepese@gmail.com>
2020-09-23 12:18:14 +0200

    JS funziona! aggiungere addclass x paypall

--- script.js
+++ script.js
@@ -6,5 +6,26 @@ $(document).ready(function (){
     var testo = $(this). children("option:selected"). val();
 
   console.log(testo);
+
+    if( testo == 1){
+      console.log("FUNZIONA 1")
+    };
+
+    if( testo == 2){
+    console.log("FUNZIONA 2")
+  };
+
+    if( testo == 4){
+    console.log("FUNZIONA 4")
+  };
+
+    if( testo == 8){
+    console.log("FUNZIONA 8")
+  };
+  
+    if( testo == 12){
+    console.log("FUNZIONA 12")
+  };
+
   });
 });
